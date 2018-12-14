4769ffae5d50ff30faa8ceb241af0f94e3e1b2ec
adammartin <ebondark.od@gmail.com>
2018-12-14 09:11:19 -0500

    Adding error messages to force setting of build variables.

--- bin/build_variables.sh
+++ bin/build_variables.sh
@@ -12,7 +12,22 @@ set_dirs () {
 set_dirs
 
 export MIN_COVERAGE_PERCENTAGE=0
-export BASE_IMAGE=[REPLACE_ME_WITH_IMAGE_YOU_WANT_AS_BASE]
-export DOCKER_REPO=[DOCKER_REPO_YOU_WILL_COMMIT_TO]
-export PROJECT_NAME=[YOUR_PROJECT_USED_TO_NAME_IMAGE]
+export BASE_IMAGE=
+export DOCKER_REPO=
+export PROJECT_NAME=
 export CONTAINER_NAME="$DOCKER_REPO/$PROJECT_NAME"
+
+if [ -z "$BASE_IMAGE" ]; then
+  echo "ERROR: You must set BASE_IMAGE in bin/build_variables.sh to the desired base docker image you are building on." 1>&2
+  exit 64
+fi
+
+if [ -z "$DOCKER_REPO" ]; then
+  echo "ERROR: You must set DOCKER_REPO in bin/build_variables.sh to the desired base docker repo you will push to." 1>&2
+  exit 64
+fi
+
+if [ -z "$PROJECT_NAME" ]; then
+  echo "ERROR: You must set PROJECT_NAME in bin/build_variables.sh to the desired base docker image name you are building." 1>&2
+  exit 64
+fi
