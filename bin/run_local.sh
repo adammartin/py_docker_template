21baad4a1b39632583829e2cd3cbf681f8ffe196
adammartin <ebondark.od@gmail.com>
2018-12-14 11:01:41 -0500

    Updating to remove the last of babelfish references

--- bin/run_local.sh
+++ bin/run_local.sh
@@ -5,4 +5,5 @@ cd "${0%/*}"
 
 . "build_variables.sh"
 
-docker run -ti -v "$PROJECT_DIR/app":/app "$CONTAINER_NAME":latest /bin/bash
+# docker run -ti -v "$PROJECT_DIR/app":/app "$CONTAINER_NAME":latest /bin/bash
+docker run -i -p 80:80 "$CONTAINER_NAME":latest
